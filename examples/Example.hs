1c5e51128a043e5111f9d8b80e2ab9fbe2311a3e
Nicolas Mattia <nicolas@nmattia.com>
2016-06-20 10:46:18 +0100

    Add Time module

--- examples/Example.hs
+++ examples/Example.hs
@@ -5,6 +5,7 @@ import           Control.Lens
 import           Control.Monad       (foldM, replicateM_, void)
 import           Data.Proxy
 import           Halytics.Monitor
+import           Halytics.Time
 import           Server              (Server (..), fastSum, request, slowSum)
 import           Statistics.Sample   (Sample)
 import           System.Random       (randomRIO)
@@ -23,15 +24,10 @@ type Benchmarker =
            , L (Max |^ Last 10)]])
 
 main :: IO ()
-main = flop
-  where
-    numberOfRequests = 10000
-
-flop :: IO ()
-flop = do
-    ms' <- foldM (\mo _ -> notify mo <$> performARequest) m0 [1.. 1000]
-    ms'^._1 & (putStrLn . result)
-    ms'^._4._2 & (putStrLn . result)
+main = do
+    ms <- foldM (\mo _ -> notify mo <$> performARequest) m0 [1.. 1000]
+    ms^._1 & (putStrLn . result)
+    ms^._4._2 & (putStrLn . result)
   where
     m0 = g' :: Monitor Benchmarker
 
@@ -40,20 +36,10 @@ flop = do
 performARequest :: IO Double
 performARequest = do
   input <- randomRIO (1000, 100000)
-  (_result, us) <- measure $ request (Server slowSum) input
+  (_result, us) <- timeIO $ request (Server slowSum) input
   return us
 
 servers :: [(ServerID, Server)]
 servers = zip [1 .. ] [slowServer, fastServer, slowServer]
   where fastServer = Server fastSum
         slowServer = Server slowSum
-
-timeSpecAsMicroSecs :: Clk.TimeSpec -> Double
-timeSpecAsMicroSecs t = fromIntegral (Clk.timeSpecAsNanoSecs t)  / 1e3
-
-measure :: IO a -> IO (a, Double)
-measure f = do
-  was <- Clk.getTime Clk.Monotonic
-  res <- f
-  is <- Clk.getTime Clk.Monotonic
-  return (res, timeSpecAsMicroSecs $ Clk.diffTimeSpec was is)
---
+++ src/Halytics/Time.hs
@@ -0,0 +1,13 @@
+module Halytics.Time where
+
+import qualified System.Clock        as Clk
+
+timeSpecAsMicroSecs :: Clk.TimeSpec -> Double
+timeSpecAsMicroSecs t = fromIntegral (Clk.timeSpecAsNanoSecs t)  / 1e3
+
+timeIO :: IO a -> IO (a, Double)
+timeIO f = do
+  was <- Clk.getTime Clk.Monotonic
+  res <- f
+  is <- Clk.getTime Clk.Monotonic
+  return (res, timeSpecAsMicroSecs $ Clk.diffTimeSpec was is)
