27dc50ce9696e256a3764694b24861f3d321c796
Nicolas Mattia <nicolas@nmattia.com>
2016-06-26 15:09:35 +0100

    Work around clock deprecation

--- src/Halytics/Time.hs
+++ src/Halytics/Time.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE CPP #-}
+
 module Halytics.Time where
 
 import qualified System.Clock as Clk
 
 timeSpecAsMicroSecs :: Clk.TimeSpec -> Double
+#if MIN_VERSION_clock(0,7,0)
 timeSpecAsMicroSecs t = fromIntegral (Clk.toNanoSecs t)  / 1e3
+#else
+timeSpecAsMicroSecs t = fromIntegral (Clk.timeSpecAsNanoSecs t)  / 1e3
+#endif
 
 timeIO :: IO a -> IO (a, Double)
 timeIO f = do
